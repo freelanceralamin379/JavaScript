693382e8513031a13633ae2c237f974f195fcf2a
freelanceralamin379 <freelanceralamin379@gmail.com>
2023-09-10 00:27:55 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var naam = 'Al amin'
+var age = 30
+
+var num1 = 4
+var num2 = 8
+
+var total = num1 + num2
+
+var multiply = num1 * num2
+
+var divide = num2 / num1
+
+var subs = num2 - num1
+
+console.log('My name is ' + naam);
+console.log('My age is ' + age);
+console.log('I am Al amin. My age is 30')
+console.log('I am ' + naam + ' My age is ' + age)
+
+
+console.log('Total is: ' + total);
+console.log('Final output is: ' + multiply)
+console.log('Final output is: ' + divide)
+console.log('Final output is: ' + subs)
+
+console.log(total + multiply + divide + subs)
+console.log("Addition: " + total + " multipliction: " + multiply + " Divition: " + divide + " Substrction: " + subs)
